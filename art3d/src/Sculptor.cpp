a948de00623ffcdd23f4a5bd3fb24e9561fceb94
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-10-07 09:42:52 -0300

    Add files via upload

---
+++ art3d/src/Sculptor.cpp
@@ -0,0 +1,55 @@
+#include "Sculptor.h"
+
+/*Sculptor::Sculptor()
+{
+    //ctor
+}
+
+Sculptor::~Sculptor()
+{
+    //dtor
+}*/
+
+Sculptor::Sculptor(int _nx, int _ny, int _nz){ //alocando a matriz 3d dinamicamente
+
+}
+
+void Sculptor::setColor(float r, float g, float b, float a){
+
+}
+
+void Sculptor::putVoxel(int x, int y, int z){
+
+}
+
+void Sculptor::cutVoxel(int x, int y, int z){
+
+}
+
+void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1){
+
+}
+
+void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
+
+}
+
+void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
+
+}
+
+void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
+
+}
+
+void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
+
+}
+
+void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
+
+}
+
+void Sculptor::writeOFF(const char* filename){
+
+}
---
+++ art3d/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Sculptor.h"
+
+struct Voxel{
+    float r, g, b; // cores: vermelho, verde e azul
+    float a; //transparencia
+    bool isOn; // "aparecer"
+};
+
+int main(void){
+
+
+    return 0;
+}
+
