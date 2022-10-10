a8c55f99b3bfdc8121bb8a9aba338c4b85158b57
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-10-10 08:39:07 -0300

    Update main.cpp

--- art3d/src/main.cpp
+++ art3d/src/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include "Sculptor.h"
+#include <iostream>
 
-int main(void){
+using namespace std;
+
+int main()
+{
+    Sculptor s(20,20,20);
+    s.setColor(100,250,75,1.0);
+    s.putBox(0,10,2,5,3,3);
+
+    s.print_pro(1,1,1);
+    //std::cout << " " << std::endl;
+    //s.print_pro(1,0,0);
 
 
     return 0;
 }
-
