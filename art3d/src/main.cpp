5c3160efa8026e88bc7505b5f4d5aeda1c03d877
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-10-19 10:35:48 -0300

    Update main.cpp

--- art3d/src/main.cpp
+++ art3d/src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include "Sculptor.h"
 #include <iostream>
@@ -6,12 +7,38 @@ using namespace std;
 
 int main()
 {
-    Sculptor s(3,3,5);
-    s.setColor(100,250,75,1.0);
-    s.putBox(1,2,1,2,0,4);
+    Sculptor mario(24,32,1);
+    mario.setColor(1.0,0,0,1.0);
+    mario.putBox(0,1,3,8,0,0);
+    mario.putVoxel(1,2,0);
+    mario.putBox(1,1,9,11,0,0);
+    mario.putBox(8,9,1,3,0,0);
+    mario.cutVoxel(9,0,0);
+    mario.putVoxel(10,2,0);
+    mario.putBox(7,7,2,3,0,0);
+    mario.setColor(1.0,0.95,0.45,1.0);
+    mario.putBox(2,6,5,7,0,0);
+    mario.putBox(5,6,3,4,0,0);
+    mario.putBox(3,4,2,2,0,0);
+    mario.putVoxel(3,4,0);
+    mario.putBox(6,6,8,10,0,0);
+    mario.putVoxel(4,8,0);
+    mario.putBox(2,3,9,9,0,0);
+    mario.putBox(3,4,10,11,0,0);
+    mario.putVoxel(4,12,0);
+    //olho e bigode
+    mario.setColor(0,0,0,1.0);
+    mario.putBox(2,3,8,8,0,0);
+    mario.putBox(5,5,8,11,0,0);
+    mario.putVoxel(4,9,0);
+    //cabelo
+    mario.setColor(1,0.6,0.2,1.0);
+    mario.putBox(3,5,1,1,0,0);
+    mario.putBox(2,2,2,4,0,0);
+    mario.putBox(3,4,3,3,0,0);
+    mario.putVoxel(4,4,0);
 
-    //s.print_pro(1,1,1);
-    s.writeOFF("teste2.txt");
+    mario.writeOFF("cabelo_mario1.off");
 
 
     return 0;
