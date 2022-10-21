a7bfbb9eaf1a21120031f1132bbce0a0be419a04
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-10-21 14:47:33 -0300

    Update Sculptor.h

--- art3d/include/Sculptor.h
+++ art3d/include/Sculptor.h
@@ -20,7 +20,7 @@ class Sculptor
     public:
       /**
        * @brief Sculptor é o construtor da classe Sculptor
-       * @param _nx é o numero de linhas da matriz 3d
+       * @param _nx é o número de linhas da matriz 3d
        * @param _ny é o número de colunas da matriz 3d
        * @param _nz é o número de planos da matriz 3d
        */
@@ -28,85 +28,85 @@ class Sculptor
       ~Sculptor();
       /**
        * @brief setColor é o metódo para definir a cor que será usada na criação da estrutura.
-       * @param r é o parametro para o vermelho - 0 até 1.
-       * @param g é o parametro para o verde - 0 até 1.
-       * @param b é o parametro para o azul - 0 até 1.
-       * @param a é o parametro para o alfa (visibilidade) - 0 até 1.
+       * @param r é o parâmetro para o vermelho - 0 até 1.
+       * @param g é o parâmetro para o verde - 0 até 1.
+       * @param b é o parâmetro para o azul - 0 até 1.
+       * @param a é o parâmetro para o alfa (visibilidade) - 0 até 1.
        */
       void setColor(float r, float g, float b, float a);
       /**
-       * @brief putVoxel
-       * @param x
-       * @param y
-       * @param z
+       * @brief putVoxel é o metódo para criar um elemento de voxel.
+       * @param x é o parâmetro para a posição x do voxel.
+       * @param y é o parâmetro para a posição y do voxel.
+       * @param z é o parâmetro para a posição z do voxel.
        */
       void putVoxel(int x, int y, int z);
       /**
-       * @brief cutVoxel
-       * @param x
-       * @param y
-       * @param z
+       * @brief cutVoxel é o metódo para "apagar" o voxel;
+       * @param x é o parâmetro para a posição x do voxel.
+       * @param y é o parâmetro para a posição y do voxel.
+       * @param z é o parâmetro para a posição z do voxel.
        */
       void cutVoxel(int x, int y, int z);
       /**
-       * @brief putBox
-       * @param x0
-       * @param x1
-       * @param y0
-       * @param y1
-       * @param z0
-       * @param z1
+       * @brief putBox é o metódo para criar uma caixa de voxels
+       * @param x0 é o parâmetro x inicial da caixa.
+       * @param x1 é o parâmetro x final da caixa.
+       * @param y0 é o parâmetro y inicial da caixa.
+       * @param y1 é o parâmetro y final da caixa.
+       * @param z0 é o parâmetro z inicial da caixa.
+       * @param z1 é o parâmetro z final da caixa.
        */
       void putBox(int x0, int x1, int y0, int y1, int z0, int z1);
       /**
-       * @brief cutBox
-       * @param x0
-       * @param x1
-       * @param y0
-       * @param y1
-       * @param z0
-       * @param z1
+       * @brief cutBox é o metódo para "apagar" uma caixa de voxels.
+       * @param x0 é o parâmetro x inicial da caixa.
+       * @param x1 é o parâmetro x final da caixa.
+       * @param y0 é o parâmetro y inicial da caixa.
+       * @param y1 é o parâmetro y final da caixa.
+       * @param z0 é o parâmetro z inicial da caixa.
+       * @param z1 é o parâmetro z final da caixa.
        */
       void cutBox(int x0, int x1, int y0, int y1, int z0, int z1);
       /**
-       * @brief putSphere
-       * @param xcenter
-       * @param ycenter
-       * @param zcenter
-       * @param radius
+       * @brief putSphere é o metódo para criar uma esfera de voxels.
+       * @param xcenter é o parâmetro que define o centro da esfera em x
+       * @param ycenter é o parâmetro que define o centro da esfera em y
+       * @param zcenter é o parâmetro que define o centro da esfera em z
+       * @param radius é o parâmetro que define o tamanho do raio da esfera
        */
       void putSphere(int xcenter, int ycenter, int zcenter, int radius);
       /**
-       * @brief cutSphere
-       * @param xcenter
-       * @param ycenter
-       * @param zcenter
-       * @param radius
+       * @brief cutSphere é o metódo para "apagar" uma esfera de voxels
+       * @param xcenter é o parâmetro que define o centro da esfera em x
+       * @param ycenter é o parâmetro que define o centro da esfera em y
+       * @param zcenter é o parâmetro que define o centro da esfera em z
+       * @param radius é o parâmetro que define o tamanho do raio da esfera
        */
       void cutSphere(int xcenter, int ycenter, int zcenter, int radius);
       /**
-       * @brief putEllipsoid
-       * @param xcenter
-       * @param ycenter
-       * @param zcenter
-       * @param rx
-       * @param ry
-       * @param rz
+       * @brief putEllipsoid é o metódo para definir um elipsoide de voxels
+       * @param xcenter é o parâmetro que define o centro do elipsoide em x
+       * @param ycenter é o parâmetro que define o centro do elipsoide em y
+       * @param zcenter é o parâmetro que define o centro do elipsoide em z
+       * @param rx é o parâmetro que define o tamanho do raio da elipsoide em torno do eixo x
+       * @param ry é o parâmetro que define o tamanho do raio da elipsoide em torno do eixo y
+       * @param rz é o parâmetro que define o tamanho do raio da elipsoide em torno do eixo z
        */
       void putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
       /**
-       * @brief cutEllipsoid
-       * @param xcenter
-       * @param ycenter
-       * @param zcenter
-       * @param rx
-       * @param ry
-       * @param rz
+       * @brief cutEllipsoid é o metódo para "apagar" um elipsoide de voxels
+       * @param xcenter é o parâmetro que define o centro do elipsoide em x
+       * @param ycenter é o parâmetro que define o centro do elipsoide em y
+       * @param zcenter é o parâmetro que define o centro do elipsoide em z
+       * @param rx é o parâmetro que define o tamanho do raio da elipsoide em torno do eixo x
+       * @param ry é o parâmetro que define o tamanho do raio da elipsoide em torno do eixo y
+       * @param rz é o parâmetro que define o tamanho do raio da elipsoide em torno do eixo z
        */
       void cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
       /**
-       * @brief writeOFF
-       * @param filename
+       * @brief writeOFF é o metódo que transcreverá todos os verticíes e faces aplicados para o formato (OFF).
+       * @param filename é o arquivo de saída no formato off resultante
        */
       void writeOFF(const char* filename);
 };
